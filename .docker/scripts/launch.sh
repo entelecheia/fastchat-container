86ba5944b8879fa50d55ba6c2367f51511d959da
Young Joon Lee <entelecheia@hotmail.com>
2023-08-28 19:52:39 +0900

    feat: initial commit

---
+++ .docker/scripts/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# add your custom commands here that should be executed every time the docker container starts
+echo "Starting docker container..."
+
+# Clones the app repository from GitHub
