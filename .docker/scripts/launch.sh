a6e419aacdf9438dd52d04dd53fd4017ac567b37
Young Joon Lee <entelecheia@hotmail.com>
2023-08-30 04:27:36 +0900

    chore(docker-config): update commit version, rename app_server_name, add build_images_from_dockerfile, docker_service_name use_jupyter, use_ssh_service and use_web_service flags

--- .docker/scripts/launch.sh
+++ .docker/scripts/launch.sh
@@ -2,4 +2,21 @@
 # add your custom commands here that should be executed every time the docker container starts
 echo "Starting docker container..."
 
+# start ssh server
+sudo service ssh start
 # Clones the app repository from GitHub
+# start jupyter notebook in background and redirect output to logfile
+# change working directory to workspace root
+# set token to value of JUPYTER_TOKEN
+# set port to value of JUPYTER_DOCKER_PORT
+if [[ -z "$(command -v jupyter)" ]]; then
+    echo "Jupyter not installed. Exiting..."
+    exit 1
+fi
+jupyter lab \
+    --no-browser \
+    --notebook-dir="$WORKSPACE_ROOT" \
+    --ServerApp.token="$JUPYTER_TOKEN" \
+    --port="$JUPYTER_PORT" \
+    --ip=0.0.0.0 \
+    --allow-root
